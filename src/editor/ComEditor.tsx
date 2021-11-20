27734ded3448c7cf09709e0cf35697cf8cd9cc55
aengun <aengun@icloud.com>
2021-11-20 16:57:45 +0900

    refactor: 테스트용 메세지 추가

--- src/editor/ComEditor.tsx
+++ src/editor/ComEditor.tsx
@@ -4,11 +4,13 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 
 const ComEditor = () => {
   const editorRef: any = useRef(null);
-  const current: any = editorRef.current.getInstance();
-  const markdown = current.getMarkdown();
-  const HTML = current.getHTML();
+
+  const testValue = `<h2>What is Lorem Ipsum?</h2><p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a <strong>type specimen book</strong>. It has <del>survived</del> not only five <em>centuries</em>, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p><ul><li><p>number 1</p></li><li><p>number 2</p></li></ul><p><br class="ProseMirror-trailingBreak"></p><ol><li><p>number1</p></li><li><p>number2</p></li></ol><p><br class="ProseMirror-trailingBreak"></p>`;
 
   const onClickButton = () => {
+    const current: any = editorRef.current?.getInstance();
+    const markdown = current?.getMarkdown();
+    const HTML = current?.getHTML();
     console.log(markdown);
     console.log(HTML);
   };
@@ -23,6 +25,7 @@ const ComEditor = () => {
         useCommandShortcut={false}
         usageStatistics={false}
         ref={editorRef}
+        initialValue={testValue}
       />
       <button onClick={onClickButton}>CLICK!!</button>
     </>
